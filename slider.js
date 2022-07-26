fae7151f0aee3b350cba6c1416bdeffa94662923
GeorgyShustrov <gosha-s.1999@yandex.ru>
2022-07-26 10:59:16 +0300

    add comments

--- slider.js
+++ slider.js
@@ -1,43 +1,58 @@
+/**
+ *
+ * Константы HTML + массив слайдов для удобства
+ *
+ */
 const slider = document.querySelector(".slider-wrapper"),
   prevButton = document.querySelector(".button-prev"),
   nextButton = document.querySelector(".button-next"),
   dots = document.querySelectorAll(".dot"),
   items = document.querySelectorAll(".item"),
-  documentWidth = document.querySelector("body").clientWidth;
-slides = [];
-let viewport = 300;
+  documentWidth = document.querySelector("body").clientWidth,
+  slides = [];
+/**
+ *
+ * Переменные для работы:viewport-ширина слайда, maxHeight - максимальная высота слайда,
+ * x1 - координата касания/клика, xDif - расстояние от x1 до отпускания кнопки по горизонтали,
+ * activeIndex- текущий активный слайд и dot
+ *
+ */
+let viewport = 300,
+  maxHeight = 420;
+(x1 = null), (xDif = null), (activeIndex = 0);
+// у мобилок слайды уже
 if (documentWidth < 400) {
   viewport = 280;
 }
-let maxHeight = 420;
-let handleChanged = false;
-
+//собираем слайды в массив и считаем высоту
 for (let i = 0; i < items.length; i++) {
   let curHeight = items[i].getBoundingClientRect().height;
   maxHeight = curHeight > maxHeight ? curHeight : maxHeight;
   slides.push(items[i]);
   slides[i].style.left = i * viewport - viewport + "px";
 }
+//задаём высоту по самому большому слайду
 slider.style.height = maxHeight + "px";
 for (let i = 0; i < slides.length; i++) {
   slides[i].style.height = maxHeight + "px";
 }
-let activeIndex = 0;
+// добавляем класс active для первого слайда и точки
 activeSlide(activeIndex);
-
-let autoSwipeId = autoSwipe();
+//включаем автоматическую прокрутку
+var autoSwipeId = autoSwipe();
+/**
+ *
+ * SLIDER FUNCTIONS
+ *
+ */
 //показать следующий слайд
 const nextSlide = () => {
   handleChanged = true;
-  nextButton.removeEventListener("click", nextButtonClick);
-  prevButton.removeEventListener("click", prevButtonClick);
   let offset2 = 0;
-
   for (let i = 0; i < slides.length; i++) {
     slides[i].style.left = offset2 * viewport - viewport * 2 + "px";
     offset2++;
   }
-
   setTimeout(() => {
     let slide = slides.shift();
     slide.remove();
@@ -47,19 +62,12 @@ const nextSlide = () => {
     nextButton.addEventListener("click", nextButtonClick);
     prevButton.addEventListener("click", prevButtonClick);
   }, 500);
-
   activeIndex++;
   activeSlide(activeIndex);
 };
-function nextButtonClick() {
-  clearInterval(autoSwipeId);
-  nextSlide();
-  autoSwipeId = autoSwipe();
-}
+
 //показать предыдущий слайд
 const prevSlide = () => {
-  nextButton.removeEventListener("click", nextButtonClick);
-  prevButton.removeEventListener("click", prevButtonClick);
   let offset2 = 0;
   let slide = slides.pop();
   slide.remove();
@@ -79,11 +87,7 @@ const prevSlide = () => {
     prevButton.addEventListener("click", prevButtonClick);
   }, 500);
 };
-function prevButtonClick() {
-  clearInterval(autoSwipeId);
-  prevSlide();
-  autoSwipeId = autoSwipe();
-}
+
 //по индексу переставляем класс active
 function activeSlide(index) {
   if (index < 0) {
@@ -101,28 +105,61 @@ function activeSlide(index) {
   items[activeItem].classList.add("active");
   dots[index].classList.add("active");
 }
-//привязываем события на кнопки
-nextButton.addEventListener("click", nextSlide);
-prevButton.addEventListener("click", prevSlide);
 function autoSwipe() {
+  //дополнительная проверка на отсутствие работающего интервала
+  if (autoSwipeId) clearInterval(autoSwipeId);
   let intervalId = setInterval(() => {
+    nextButton.removeEventListener("click", nextButtonClick);
+    prevButton.removeEventListener("click", prevButtonClick);
     nextSlide();
-  }, 4000);
+  }, 4500);
   return intervalId;
 }
-
-let x1 = null,
-  xDif = null;
-
-slider.addEventListener("mousedown", onMouseDown, false);
-slider.addEventListener("mousemove", onMouseMove, false);
-slider.addEventListener("mouseup", onMouseUp, false);
-slider.addEventListener("mouseleave", onMouseUp, false);
+function stopAutoSwipe() {
+  clearInterval(autoSwipeId);
+  autoSwipeId = null;
+}
+/**
+ *
+ * BUTTONS EVENTS LISTENERS
+ *
+ */
+nextButton.addEventListener("click", nextButtonClick);
+prevButton.addEventListener("click", prevButtonClick);
+/**
+ *
+ * BUTTONS EVENTS
+ *
+ */
+function nextButtonClick() {
+  nextButton.removeEventListener("click", nextButtonClick);
+  prevButton.removeEventListener("click", prevButtonClick);
+  stopAutoSwipe();
+  nextSlide();
+  autoSwipeId = autoSwipe();
+}
+function prevButtonClick() {
+  nextButton.removeEventListener("click", nextButtonClick);
+  prevButton.removeEventListener("click", prevButtonClick);
+  stopAutoSwipe();
+  prevSlide();
+  autoSwipeId = autoSwipe();
+}
+/**
+ *
+ * TOUCHSCREEN EVENTS LISTENERS
+ *
+ */
 slider.addEventListener("touchstart", handleTouchStart, false);
 slider.addEventListener("touchmove", handleTouchMove, false);
 slider.addEventListener("touchend", handleTouchEnd, false);
+/**
+ *
+ * TOUCHSCREEN EVENTS
+ *
+ */
 function handleTouchStart(event) {
-  clearInterval(autoSwipeId);
+  stopAutoSwipe();
   const firstTouch = event.touches[0];
   x1 = firstTouch.clientX;
   handleChanged = true;
@@ -137,7 +174,7 @@ function handleTouchMove(event) {
     slides[i].style.left = i * viewport - viewport + xDif * 2 + "px";
   }
 }
-function handleTouchEnd(event) {
+function handleTouchEnd() {
   if (xDif <= -75) {
     nextSlide();
   } else if (xDif >= 75) {
@@ -151,8 +188,22 @@ function handleTouchEnd(event) {
   x1 = null;
   xDif = null;
 }
+/**
+ *
+ * MOUSE EVENTS LISTENERS
+ *
+ */
+slider.addEventListener("mousedown", onMouseDown, false);
+slider.addEventListener("mousemove", onMouseMove, false);
+slider.addEventListener("mouseup", onMouseUp, false);
+slider.addEventListener("mouseleave", onMouseUp, false);
+/**
+ *
+ * MOUSE EVENTS
+ *
+ */
 function onMouseDown(event) {
-  clearInterval(autoSwipeId);
+  stopAutoSwipe();
   const firstTouch = event.clientX;
   x1 = firstTouch;
 }
@@ -166,7 +217,7 @@ function onMouseMove(event) {
     slides[i].style.left = i * viewport - viewport + xDif * 1.5 + "px";
   }
 }
-function onMouseUp(event) {
+function onMouseUp() {
   if (xDif <= -150) {
     nextSlide();
   } else if (xDif >= 150) {
@@ -178,5 +229,6 @@ function onMouseUp(event) {
   }
   x1 = null;
   xDif = null;
+
   autoSwipeId = autoSwipe();
 }
